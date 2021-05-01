f102e28f5be0d3a14d563ddd82616c5690c4c763
HoKhanhDuong <10dragonball.5onepice.1naurto@gmail.com>
2021-05-01 08:15:45 +0700

    full

--- src/HangHoa/main.java
+++ src/HangHoa/main.java
@@ -28,6 +28,38 @@ public class main {
 	    System.out.println("------------/---------------/------------"); 
 	}
 
+	private static ArrayList<HangHoa> importList(){
+		int number;
+		Scanner sc = new Scanner(System.in);
+
+		ArrayList<HangHoa> list = new ArrayList<HangHoa>();
+
+		System.out.println("So luong hang muon nhap\n");
+		number = sc.nextInt();
+		int c;
+
+		for(int i=0; i<number; i++){
+			System.out.println("1. Nhập hàng thực phẩm (bao gồm giá)\n"
+								+ "2. Nhập hàng điện tử (bao gồm giá)\n"
+								+ "Nhập lựa chọn của bạn: ");
+			c = sc.nextInt();
+			if(c==1){
+				HangThucPham tp = new HangThucPham();
+				tp.nhap();
+				list.add(tp);
+			}else {
+				HangDienTu dt = new HangDienTu();
+				dt.nhap();
+				list.add(dt);
+			}
+		}
+
+		for(int i=0; i < list.size(); i++){
+			System.out.println("sản phẩm "+i+": "+list.get(i).toString()+"\n");
+		}
+		return list;
+	}
+
 	public static void main(String[] args) {
 
 		ArrayList<HangHoa> list = new ArrayList<HangHoa>();
@@ -39,8 +71,8 @@ public class main {
 		do {
 		System.out.println("1. Nhập, xuất một hàng thực phẩm (bao gồm giá)\n"
 				+ "2. Nhập, xuất một hàng điện tử (bao gồm giá)\n"
-				+ "3. Xây dựng lớp CỬA HÀNG để quản lý danh sách các sản phẩm\n"
-				+ "4. Nhập xuất danh sách các sản phẩm\n"
+				+ "3. Danh sách các sản phẩm\n"
+				+ "4. Nhập, xuất danh sách các sản phẩm\n"
 				+ "5. Tìm sản phẩm có giá thấp nhất trong danh sách \n"
 				+ "Nhập lựa chọn của bạn: "
 				);
@@ -49,14 +81,31 @@ public class main {
 		switch (a) {
 		case 1:
 			HangThucPham(tp);
+			list.add(tp);
 			break;
 		case 2:
 			HangDienTu(dt);
+			list.add(dt);
 			break;
 		case 3:
-	
+			for(int i=0; i < list.size(); i++){
+				System.out.println("sản phẩm "+i+": "+list.get(i).toString()+"\n");
+			}
 			break;
-
+		case 4:
+			list.addAll(importList());
+			break;
+		case 5:
+			float min = list.get(0).getGia();
+			int index=0;
+			for(int i=0; i < list.size(); i++){
+				if(min > list.get(i).getGia()){
+					index = i;
+					min = list.get(i).getGia();
+				}
+			}
+			System.out.println("San pham co gia thap nhat : "+list.get(index).toString());
+			break;
 		default:
 			break;
 		}
